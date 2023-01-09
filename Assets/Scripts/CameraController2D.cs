8a0ab3f8ca69712749585dde904dc4915909a2cf
FireFart <64144285+lukky09@users.noreply.github.com>
2023-01-10 03:47:26 +0700

    Mulai mbuat Kamera

---
+++ Assets/Scripts/CameraController2D.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraController2D : MonoBehaviour
+{
+    [SerializeField] GameObject FloorTilemapObject;
+
+    Tilemap FloorTilemapReference;
+    float[] tilesize; // width, height
+    // Start is called before the first frame update
+    void Start()
+    {
+        FloorTilemapReference = FloorTilemapObject.GetComponent<Tilemap>();
+        tilesize = new float[2] { 0, Camera.main.orthographicSize };
+        tilesize[0] = tilesize[1] * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position.x);
+        float x = Mathf.Clamp(transform.position.x, tilesize[0], SetStones.getWidth() - tilesize[0]);
+        float y = Mathf.Clamp(transform.position.y, tilesize[1], -SetStones.getHeight() + tilesize[1]);
+        transform.position = new Vector3(x, y);
+    }
+}
