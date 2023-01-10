42bdf68822c773245e3eb17a5130d41b8c40c9cb
FireFart <64144285+lukky09@users.noreply.github.com>
2023-01-10 14:55:52 +0700

    imlazy

--- Assets/Scripts/CameraController2D.cs
+++ Assets/Scripts/CameraController2D.cs
@@ -20,9 +20,10 @@ public class CameraController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position.x);
+
         float x = Mathf.Clamp(transform.position.x, tilesize[0], SetStones.getWidth() - tilesize[0]);
         float y = Mathf.Clamp(transform.position.y, tilesize[1], -SetStones.getHeight() + tilesize[1]);
+        Debug.Log(transform.position.x + " " + tilesize[0] + " " + (SetStones.getWidth() - tilesize[0]));
         transform.position = new Vector3(x, y);
     }
 }
