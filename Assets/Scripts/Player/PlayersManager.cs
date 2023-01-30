8a35c7c7f75e3a31020103a26cb85cd8bc33e810
FireFart <64144285+lukky09@users.noreply.github.com>
2023-01-30 14:39:13 +0700

    Fix Bug + tambah struktur Buku

--- Assets/Scripts/Player/PlayersManager.cs
+++ Assets/Scripts/Player/PlayersManager.cs
@@ -17,7 +17,6 @@ public class PlayersManager : MonoBehaviour
         Coordinate coor =  Coordinate.getRandomCoordinate();
         makeNewPlayer(coor);
         makeNewBot(new Coordinate(SetObjects.getWidth() - coor.xCoor, coor.yCoor),false);
-
         for (int i = 1; i < 5 - 4; i++)
         {
             coor = Coordinate.getRandomCoordinate();
@@ -33,20 +32,24 @@ public class PlayersManager : MonoBehaviour
             if (players[i] == null)
             {
                 players[i] = Instantiate(playerPrefab, c.returnAsVector(),Quaternion.identity);
-                levelCamera.GetComponent<CameraController2D>().setCameraFollower(playerPrefab);
+                levelCamera.GetComponent<CameraController2D>().setCameraFollower(players[i]);
+                break;
             }
         }
     }
 
     public void makeNewBot(Coordinate c, bool isPlayerTeam)
     {
+        Material m = new Material(playerMaterial);
+        m.SetColor("OutlineColor",Color.magenta);
         for (int i = 0; i < players.Length; i++)
         {
             if (players[i] == null)
             {
                 GameObject tempEnemyPrefab = Instantiate(enemyPrefab, c.returnAsVector(), Quaternion.identity);
                 tempEnemyPrefab.GetComponent<SnowBrawler>().initializeBrawler(isPlayerTeam, i);
-                tempEnemyPrefab.GetComponent<SpriteRenderer>().color = Color.magenta;
+                tempEnemyPrefab.GetComponent<SpriteRenderer>().material = new Material(m);
+                break;
             }
         }
     }
